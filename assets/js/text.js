bf235bf1df0057665edb5159c0a69f49e71f127c
kamlgks <kamoliddintuhtaboev2@gmail.com>
2024-06-11 20:30:33 +0300

    F

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,21 +1,18 @@
-$(function () { 
-    var title = document.title 
-        , animSeq = ["/", "/", "\\", "|", "/"] 
-        , animIndex = 0 
-        , titleIndex = 0; 
- 
-    function doInverseSpinZeroPitch() { 
-        var loadTitle = title.substring(0, titleIndex); 
-        if (titleIndex > title.length) { 
-            animIndex = 0; 
-            titleIndex = 0 
-        } 
-        if (animIndex > 3) { 
-            titleIndex++; 
-            animIndex = 0 
-        } 
-        document.title = loadTitle + animSeq[animIndex]; 
-        animIndex++ 
-    } 
-    window.setInterval(doInverseSpinZeroPitch, 150); 
+$(function () {
+    var title = document.title,
+        animSeq = ["/", "-", "\\", "|"],
+        animIndex = 0,
+        titleIndex = 0;
+
+    function doInverseSpinZeroPitch() {
+        var loadTitle = title.substring(0, titleIndex);
+        if (titleIndex >= title.length) {
+            titleIndex = 0;
+        }
+        document.title = loadTitle + animSeq[animIndex];
+        animIndex = (animIndex + 1) % animSeq.length;
+        titleIndex++;
+    }
+
+    window.setInterval(doInverseSpinZeroPitch, 150);
 });
